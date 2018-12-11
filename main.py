3ec8ce177c4b4d5eeae2bcd0619ef014f7e3e97b
nesc <limarevigor@gmail.com>
2018-12-11 21:16:22 +0200

    add chaotic method

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-import SierpinskiTriangle
+from SierpinskiTriangle import *
 
 
 def main():
-    obj = SierpinskiTriangle()
-    obj.draw()
+    obj1 = SierpinskiTriangle()
+    obj1.draw(method='iterative', degree_of_deep=5)
+    obj2 = SierpinskiTriangle()
+    obj2.draw(method='chaotic', drawing_speed=10000000)
 
 
 if __name__ == "__main__":
